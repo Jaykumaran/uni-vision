d4f48fecc64c62e128dba72f574f838c5801fa13
Jaykumaran R <r1ajayakumaran1751994@gmail.com>
2025-01-30 09:15:55 +0530

    seg-dataclass-prep

--- uni_vision/pipe/segmentation/engine/custom_dataclass.py
+++ uni_vision/pipe/segmentation/engine/custom_dataclass.py
@@ -3,6 +3,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torch.utils.data import Dataset
 import cv2
+import numpy as np
 
 class CustomSegDataset(Dataset):
     
@@ -26,7 +27,8 @@ class CustomSegDataset(Dataset):
         if mask:
             file = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
         else:
-            file = cv2.cvtColor(cv2.imread(file_path), cv2.COLOR_BAYER_BGR2RGB) #BGR 2 RGB
+            file = cv2.cvtColor(cv2.imread(file_path, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB) #BGR 2 RGB
+            
         file = cv2.resize(file, self.img_size, interpolation = interpolation)
     
         return file
@@ -39,6 +41,7 @@ class CustomSegDataset(Dataset):
         #Load image
         image = self.load_file(image_path, interpolation=cv2.INTER_CUBIC)
         
+        mask = None
         if self.mask_paths is not None:
             #Get mask path
             
@@ -46,4 +49,22 @@ class CustomSegDataset(Dataset):
             
             mask = self.load_file(mask_path, interpolation=cv2.INTER_NEAREST, mask = True)
             
-            #*
+            #Process the mask correctly
+            if self.num_classes == 1:
+                mask = (mask > 0).astype(np.float32) #Binary mask (0 or 1)
+            else:
+                mask = mask.astype(np.int64) #Multi class mask (integer labels)
+            
+            
+            #Apply transformations
+            if self.transforms:
+                augmented = self.transforms(image = image, mask = mask if mask is not None else None)
+                image  = augmented['image']
+                mask  = augmented['mask'] if "mask" in augmented else mask
+            
+            #Convert to tensors
+            image = torch.tensor(image, dtype=torch.float32).permute(2, 0, 1) #C, H, W
+            if mask is not None:
+                mask = torch.tensor(mask, dtype = torch.float32 if self.num_classes == 1 else torch.long)
+            
+            return image if mask is None else (image, mask)
